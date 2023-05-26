e76616e7cd3ae812e49c7110f56b53cffe67d641
matee-safi <matiullahsafibbc@gmail.com>
2023-05-26 08:27:06 +0430

    create modules and classes

---
+++ MyList.rb
@@ -0,0 +1,11 @@
+require_relative 'MyEnumerable'
+class MyList
+    include MyEnumerable
+    def initialize(*args)
+        @list = args
+    end
+
+    def each(&block)
+        @list.each(&block)
+    end
+end
